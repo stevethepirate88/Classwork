614d1562b0157fc9170a56e219b15d1600682b42
stevethepirate88 <stephen.crane.umd@gmail.com>
2019-02-05 19:23:08 -0500

    Initial Commit
    Just having some fun. LAWL.

---
+++ I_Can_Script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "I can script like the best of them"
+sleep 3s
+echo "Okay, maybe not the best of them."
+sleep 3s
+echo "Okay, probably not even with the good of them."
+sleep 3s
+echo "Alright, look, I think it's fair to say I can code, but that's about it."
+sleep 3s
+echo "I don't know what you want from me! Why are you still here?"
+sleep 3s
+echo "Oh, it's because the script is still running, huh?"
+sleep 3s
+echo "You're trapped here! Bwahahahahaahahahahahaha"
+sleep 6s
+clear
+echo "Hello, Neo"
+sleep 1s
+clear
+echo "Hello, Neo."
+sleep 1s
+clear
+echo "Hello, Neo.."
+sleep 1s
+clear
+echo "Hello, Neo..."
+sleep 3s
+echo "Okay, just kidding. I'm gonna go ahead and end this script."
+sleep 2s
+clear
+timeout 5s curl parrot.live
+clear
+echo "FIN"
